1041523ac6f622613b65ecd95d92fe6c43832d93
rafhathdana <danarafha@gmail.com>
2022-08-04 13:04:02 +0530

    initialised project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+
+const Container=styled.div``;
+const Main=styled.div``;
+const Wrapper =styled.div``;
+function App() {
+  return (
+  <Container>
+    <Menu />
+    <Main >
+      <Wrapper>
+        video
+      </Wrapper>
+    </Main>
+  </Container>
+  );
+}
+
+export default App;
