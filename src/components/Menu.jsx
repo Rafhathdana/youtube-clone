0eeb2a6ded1117fc357bf0aed5b753c167014099
rafhathdana <danarafha@gmail.com>
2022-08-13 17:35:02 +0530

    menu completed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,36 +2,70 @@ import React from 'react'
 import styled from 'styled-components'
 import wetube from "../img/logo.png"
 import HomeIcon from '@mui/icons-material/Home';
-import ExploreIcon from '@mui/icons-material/Explore';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import HistoryIcon from '@mui/icons-material/History';
+import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined';
+import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
+import LibraryMusicOutlinedIcon from '@mui/icons-material/LibraryMusicOutlined';
+import SportsOutlinedIcon from '@mui/icons-material/SportsOutlined';
+import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
+import MovieOutlinedIcon from '@mui/icons-material/MovieOutlined';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined';
+import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 const Container = styled.div`
    flex: 1;
    background-color: #020202;
-   color:white;
-   height:100vh;
+   height: 135vh;
+   color: white;
    font-size: 14px;
+   position: sticky;
+   top: 0;
 `;
 const Wrapper = styled.div`
-    padding:18px 26px;
+    padding: 18px 26px;
 `
 const Logo = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
   font-weight: bold;
-  margin-bottom:25px;
+  margin-bottom: 25px;
   `;
 const Img = styled.img`
-  height:25px;
+  height: 25px;
   `;
 const Item = styled.div`
     display: flex;
-    align-item: center;
-    gap:20px;
-    cursor:pointer ;
-
+    align-items: center;
+    gap: 20px;
+    cursor: pointer;
+    padding: 7.5px 0px;
 `
+const Hr = styled.hr`
+margin: 15px 0px;
+border: 0.5px solid #373737;
+`
+const Login=styled.div`
+`
+const Button=styled.button`
+padding: 5px 15px;
+background-color: transparent;
+border: 1px solid #3ea6ff;
+color: #3ea6ff;
+border-radius: 3px;
+font-weight: 500;
+margin-top: 10px;
+cursor: pointer;
+display: flex;
+align-items: center;
+gap: 5px;
+` 
+
 const Menu = () => {
     return (
         <Container>
@@ -44,19 +78,24 @@ const Menu = () => {
                     <HomeIcon />Home
                 </Item>
                 <Item>
-                    <ExploreIcon />Explore
                     <ExploreOutlinedIcon />Explore
                 </Item>
                 <Item>
-                    <SubscriptionsIcon />Subscriptions
                     <SubscriptionsOutlinedIcon />Subscriptions
                 </Item>
+                <Hr />
                 <Item>
                     <VideoLibraryOutlinedIcon />Library
                 </Item>
                 <Item>
                     <HistoryOutlinedIcon />History
-                </Item> 
+                </Item>
+                <Hr />
+                <Login>
+                    Sign in to like Videos , Comment and Subscribe.
+                    <Button><AccountCircleOutlinedIcon />SIGN IN</Button>
+                </Login>
+                <Hr />
                 <Item>
                     <LibraryMusicOutlinedIcon />Music
                 </Item>
@@ -73,10 +112,11 @@ const Menu = () => {
                     <ArticleOutlinedIcon />News
                 </Item>
                 <Item>
-                    <SettinsOutlinedIcon />Settings
-                </Item> 
-                <Item>
                     <LiveTvOutlinedIcon />Live
+                </Item>
+                <Hr />
+                <Item>
+                    <SettingsOutlinedIcon />Settings
                 </Item>
                 <Item>
                     <FlagOutlinedIcon />Report
@@ -84,11 +124,8 @@ const Menu = () => {
                 <Item>
                     <HelpOutlinedIcon />Help
                 </Item>
-                 <Item>
-                    <SettingsBrightnessOutlinedIcon />Light Mode
-                </Item>
                 <Item>
-                    <aOutlinedIcon />a
+                    <SettingsBrightnessOutlinedIcon />Light Mode
                 </Item>
             </Wrapper>
         </Container>
