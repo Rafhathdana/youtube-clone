dfe28bde9fd8272f4e5935a19bc64b82b742c1e6
rafhathdana <danarafha@gmail.com>
2022-08-14 12:41:19 +0530

    changed to Roboto font

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const Main = styled.div`
   background-color:${({ theme }) => theme.bg};
   `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+padding:22px 96px;`;
 function App() {
   const [darkMode, setDarkMode] = useState(true)
   return (
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,13 +10,49 @@ const Image = styled.img`
     height: 202px;
     background-color: #999999;
     `;
+const Details = styled.div`
+    display: flex;
+    margin-top: 16px;
+    gap:12px;
+    `;
+const ChannelImage = styled.img`
+    width: 36px;
+    height: 36px;
+    border-radius:50%;
+    background-color: #999999;
+    position: relative;  
+    `;
+const Texts = styled.div``;
+const Title = styled.h1`
+    font-size: 16px;
+    font-weight: 500;
+    color: ${({theme}) => theme.text};
+`;
+const ChannelName = styled.h2`
+    font-size: 14px;
+    color: ${({theme}) => theme.textSoft};
+    margin:9px 0px;
+    `;
+const Info = styled.div`
+    font-size: 14px;
+    color: ${({theme})=> theme.textSoft};
+`;
 const Card = () => {
-  return (
-    <Container>
-       <Image src="https://i.ytimg.com/an_webp/H3uPYyZjsaw/mqdefault_6s_480x270.webp?du=3000&sqp=COj24ZcG&rs=AOn4CLBnVmlgSj23LaNtLHQKAVF1ZXsB0A"/>
+    return (
+        <Container>
+            <Image src="https://i.ytimg.com/an_webp/H3uPYyZjsaw/mqdefault_6s_480x270.webp?du=3000&sqp=COj24ZcG&rs=AOn4CLBnVmlgSj23LaNtLHQKAVF1ZXsB0A" />
+            <Details>
+                <ChannelImage />
+                <Texts>
+                    <Title>Test Video</Title>
+                    <ChannelName>Tom and Jerry</ChannelName>
+                    <Info>560,908 views * 1 day ago</Info>
+                </Texts>
+            </Details>
+
         </Container>
 
-  )
+    )
 
 }
 
