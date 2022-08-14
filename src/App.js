b8e2181b50a6a77c4c14726a08aa4d9a1b95bf48
rafhathdana <danarafha@gmail.com>
2022-08-14 11:46:47 +0530

    Routes  added

--- src/App.js
+++ src/App.js
@@ -3,105 +3,47 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
-
-const Container=styled.div`
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+const Container = styled.div`
   display: flex;
   `;
-  const Main=styled.div`
+const Main = styled.div`
   flex: 7;
-  background-color:${({theme}) => theme.bg}
+  background-color:${({ theme }) => theme.bg};
   `;
-  
-  const Wrapper =styled.div``;
+
+const Wrapper = styled.div``;
 function App() {
-  const [darkMode ,setDarkMode]=useState(true)
+  const [darkMode, setDarkMode] = useState(true)
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
 
-  <Container>
-    <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
-    <Main >
-      <Navbar />
-      <Wrapper>
-
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-<h1>test</h1> 
-     </Wrapper>
-    </Main>
-  </Container>
-    </ThemeProvider>
-  );
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main >
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="video">
+                  <Route path=":id" element={<Video />} />
+                </Route>
+                </Route>
+              </Routes>
+                </Wrapper>
+              </Main>
+            </BrowserRouter>
+          </Container>
+        </ThemeProvider>
+        );
 }
 
-export default App;
+        export default App;
