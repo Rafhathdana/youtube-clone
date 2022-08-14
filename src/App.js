da9a8e13a13f1e3fc26403c023ab61a7e98e51c7
rafhathdana <danarafha@gmail.com>
2022-08-14 18:46:57 +0530

    frontend completed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ const Main = styled.div`
   `;
 
 const Wrapper = styled.div`
-padding:22px 96px;`;
+padding:22px 96px;
+`;
+
 function App() {
   const [darkMode, setDarkMode] = useState(true)
   return (
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     display:flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     height: calc(100vh-56px);
@@ -17,11 +18,42 @@ const Wrapper = styled.div`
     padding: 20px 50px;
     gap: 10px;
 `;
-const Title = styled.h1``;
-const SubTitle = styled.h2``;
-const Input = styled.input``;
-const Button = styled.button``;
-const More = styled.div``;
+const Title = styled.h1`
+    font-size: 24px;
+
+`;
+const SubTitle = styled.h2`
+    font-weight: 300;
+    font-size: 20px;
+`;
+const Input = styled.input`
+    border:1px solid ${({ theme }) => theme.soft};
+    border-radius: 3px;
+    padding: 10px;
+    background-color: transparent;
+    width: 100%;
+`;
+const Button = styled.button`
+    border-radius: 3px;
+    border:none;
+    padding: 10px 20px;
+    font-weight: 500;
+    cursor: pointer;
+    background-color: ${({theme})=>theme.soft};
+    color:${({theme})=>theme.textSoft};
+`;
+const More = styled.div`
+    display:flex;
+    font-size: 12px;
+    color:${({theme})=>theme.textSoft};
+    margin-top: 10px;
+`;
+const Links = styled.div`
+    margin-left: 50px;
+    `;
+const Link = styled.span`
+    margin-left: 30px;
+`;
 
 const SignIn = () => {
     return (
@@ -29,15 +61,23 @@ const SignIn = () => {
             <Wrapper>
                 <Title>Sign in </Title>
                 <SubTitle>to Continue to WeTube</SubTitle>
-                <Input placeholder='Username'/>
-                <Input type="password" placeholder='Password'/>
+                <Input placeholder='Username' />
+                <Input type="password" placeholder='Password' />
                 <Button>Sign in</Button>
                 <Title>or</Title>
-                <Input placeholder='Username'/>
-                <Input placeholder='Email'/>
-                <Input type="password" placeholder='Password'/>
+                <Input placeholder='Username' />
+                <Input placeholder='Email' />
+                <Input type="password" placeholder='Password' />
                 <Button>Sign up</Button>
             </Wrapper>
+            <More>
+                English(USA)
+                <Links>
+                    <Link>Help</Link>
+                    <Link>Privacy</Link>
+                    <Link>Terms</Link>
+                </Links>
+            </More>
         </Container>
     )
 }
