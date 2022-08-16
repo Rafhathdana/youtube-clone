b2602b5be092515b2e228caa6e041c9ddb03eff7
rafhathdana <danarafha@gmail.com>
2022-08-16 17:35:39 +0530

    added outer function of video

---
+++ controllers/video.js
@@ -0,0 +1,12 @@
+export const addVideo = async(req, res,next)=>{
+
+}
+export const updateVideo = async(req,res,next)=>{
+
+}
+export const deleteVideo = async(req,res,next)=>{
+
+}
+export const getVideo = async(req,res,next)=>{
+    
+}
--- routes/videos.js
+++ routes/videos.js
@@ -1,4 +1,11 @@
 import express from "express";
-import {  } from "../controllers/video.js";
+import { addVideo, deleteVideo, getVideo, updateVideo } from "../controllers/video.js";
+import { verifyTocken } from "../verifyTocken.js";
 const router = express.Router();
+
+// create a video
+router.post("/",verifyTocken,addVideo)
+router.put("/:id",verifyTocken,updateVideo)
+router.delete("/:id",verifyTocken,deleteVideo)
+router.get("/find/:id",getVideo) 
 export default router;
