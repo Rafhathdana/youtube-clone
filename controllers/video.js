7937939ffb420789ce8ea1c71bff107639246ca6
rafhathdana <danarafha@gmail.com>
2022-09-04 09:45:18 +0530

    completed videos api and all other regarding videos

--- controllers/video.js
+++ controllers/video.js
@@ -104,8 +104,9 @@ export const sub = async (req, res, next) => {
     }
 };
 export const getByTag = async (req, res, next) => {
+    const tags = req.query.tags.split(",")
     try {
-        const videos = await Video.find().sort({ views: -1 });
+        const videos = await Video.find({ tags: { $in: tags } }).limit(20);
         res.status(200).json(videos)
     }
     catch (err) {
@@ -113,8 +114,9 @@ export const getByTag = async (req, res, next) => {
     }
 };
 export const search = async (req, res, next) => {
+    const query = req.query.q
     try {
-        const videos = await Video.find().sort({ views: -1 });
+        const videos = await Video.find({ title: { $regex: query, $options: "i" } }).limit(40);
         res.status(200).json(videos)
     }
     catch (err) {
